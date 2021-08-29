fe9ee789190b18aa1129d19a6c50ae3c0562ab07
Jordi Coscolla <kozko2001@gmail.com>
2021-08-29 10:30:13 +0200

    get correct height

--- providers/twitter.ts
+++ providers/twitter.ts
@@ -1,4 +1,3 @@
-import { url } from 'inspector';
 import {Provider} from './index'
 
 class TwitterProvider implements Provider {
@@ -8,14 +7,35 @@ class TwitterProvider implements Provider {
         return url.match(this.regex) !== null;
     };
 
+    receiveMessage(event: any){
+        const height = event.data.height;
+        const id = event.data.element;
+        document.getElementById(id).setAttribute("height", height)
+    }
+
+    generateUUID(): string {
+        // https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
     embed(url: string): HTMLElement {
+        window.addEventListener("message", this.receiveMessage, false);
+
         const embed = document.createElement("iframe");
-		embed.setAttribute("width", "250")
-		embed.setAttribute("height", "550")
+        const id = this.generateUUID()
+        embed.setAttribute("id", id)
+		embed.setAttribute("width", "500")
+		embed.setAttribute("height", "100")
 		embed.setAttribute("border", "0")
 		embed.setAttribute("frameborder", "0")
 		embed.setAttribute("src", `https://twitframe.com/show?url=${encodeURI(url)}`)
 
+        embed.onload = function() {
+            this.contentWindow.postMessage({element: id, query: "height"}, "*")
+        };
 		return embed
     }
 
